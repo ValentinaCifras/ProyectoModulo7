c90eae8b46d51ddbf2478a1fbf253177958333a8
ValentinaCifras <valentina.cifras@gmail.com>
2024-06-11 23:04:44 -0400

    favicon

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import (index,register,profile,register_profile,update_profile,register_inmueble,
                     get_inmuebles,update_inmueble,contact,messages,disponibles,acerca)
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('',index, name='home'),
@@ -18,4 +20,6 @@ urlpatterns = [
     path('messages/', messages, name='mensaje'),
     path('disponibles/', disponibles, name='disponibles'),
     path('acerca/', acerca, name='acerca'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
+ 
 ]
